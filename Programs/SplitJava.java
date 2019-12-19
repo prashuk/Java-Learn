4c2693d6719585447d40edd88c0023f2ab6613c8
prashuk_macbook <prashuk.ajmera@gmail.com>
2019-12-19 16:22:54 -0500

    Update SplitJava.java

--- Programs/SplitJava.java
+++ Programs/SplitJava.java
@@ -5,7 +5,7 @@ public class SplitJava {
 
 	public static void main(String[] args) {
 		String s = "asd/ jk/  @.#$.%^&*(). ,a/sd.ads";
-		String[] ss = s.split("\\ ");
+		String[] ss = s.split("\\.");
 		for (String i : ss) {
 			System.out.println(i);
 		}
